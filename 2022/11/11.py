c48cc97d7954a8fb14989e7666f91f4e3ad74817
Pherkel <philippmerkel@outlook.com>
2022-12-11 11:57:52 +0100

    day 11 part 1

---
+++ 2022/11/11.py
@@ -0,0 +1,44 @@
+with open("2022/11/input") as input:
+    data = []
+    temp = [x.strip() for x in input.readlines()]
+    for i in range(0, 55, 7):
+        inst = {"items": 0, "op": [], "test": "0", "t": -1, "f": -1}
+        inst["items"] = [int(x.strip(",")) for x in temp[i+1].split(" ")[2:]]
+        inst["op"].append(temp[i+2].split(" ")[-2])
+        inst["op"].append(temp[i+2].split(" ")[-1])
+        inst["test"] = int(temp[i+3].split(" ")[-1])
+        inst["t"] = int(temp[i+4][-1])
+        inst["f"] = int(temp[i+5][-1])
+        data.append(inst)
+
+inspector = [0 for x in range(8)]
+
+for i in range(20):
+    for idx, monkey in enumerate(data):
+        while len(monkey["items"]) != 0:
+            match monkey["op"][0]:
+                case "+":
+                    if monkey["op"][1] == "old":
+                        monkey["items"][0] += monkey["items"][0]
+                    else:
+                        monkey["items"][0] += int(monkey["op"][1])
+                case "*":
+                    if monkey["op"][1] == "old":
+                        monkey["items"][0] *= monkey["items"][0]
+                    else:
+                        monkey["items"][0] *= int(monkey["op"][1])
+
+            monkey["items"][0] = monkey["items"][0]//3
+
+            if monkey["items"][0] % monkey["test"] == 0:
+                data[monkey["t"]]["items"].append(monkey["items"][0])
+            else:
+                data[monkey["f"]]["items"].append(monkey["items"][0])
+
+            monkey["items"].pop(0)
+            inspector[idx] += 1
+    print(f"Round: {i}, {data}")
+
+inspector.sort(reverse=True)
+# part 1
+print(inspector[0]*inspector[1])
