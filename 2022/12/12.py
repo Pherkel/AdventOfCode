b3ddb4cc863a70184968419d98e6f83de9e8f64a
Pherkel <philippmerkel@outlook.com>
2022-12-12 17:10:57 +0100

    :(

---
+++ 2022/12/12.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+with open("2022/12/input") as input:
+    data = [list(x.strip().split()[0]) for x in input.readlines()]
+
+dist = [[float("inf") for x in range(len(data[0]))] for x in range(len(data))]
+
+queue = []
+visited = [[False for x in range(len(data[0]))] for x in range(len(data))]
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if data[i][j] == "S":
+            data[i][j] = "a"
+            queue.append((i, j))
+            dist[i][j] = 0
+        if data[i][j] == "E":
+            end = (i, j)
+            data[i][j] = "z"
+
+
+def valid(source, target):
+    res = ord(data[target[0]][target[1]]) - \
+        ord(data[source[0]][source[1]]) <= 1
+    return res
+
+
+def get_neighbors(u):
+    res = []
+    for i in range(-1 if u[0] > 0 else 1, 2 if u[0] < len(data) - 1 else 0, 2):
+        res.append((u[0] + i, u[1]))
+    for j in range(-1 if u[1] > 0 else 1, 2 if u[1] < len(data[0]) - 1 else 0, 2):
+        res.append((u[0], u[1] + j))
+    return res
+
+
+while queue:
+    u = queue.pop(0)
+    for n in get_neighbors(u):
+        if valid(u, n) and not visited[n[0]][n[1]]:
+            dist[n[0]][n[1]] = dist[u[0]][u[1]] + 1
+            queue.append(n)
+    visited[u[0]][u[1]] = True
+
+
+print(dist[end[0]][end[1]])
