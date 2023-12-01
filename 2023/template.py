3f5fd041a4ed4fabac892298bc26b01676611087
Pherkel <philippmerkel@outlook.com>
2023-12-01 17:39:44 +0100

    puh

---
+++ 2023/template.py
@@ -0,0 +1,14 @@
+input = """"""
+
+
+def solution_1():
+    print("Solution 1: ")
+
+
+def solution_2():
+    print("Solution 2: ")
+
+
+if __name__ == "__main__":
+    solution_1()
+    solution_2()
