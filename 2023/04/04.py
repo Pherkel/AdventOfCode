00f5edb7ef53d3e7e7f98914d7528e9b9aded6b8
Pherkel <philippmerkel@outlook.com>
2023-12-04 21:10:50 +0100

    04/2

--- 2023/04/04.py
+++ 2023/04/04.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
+
+
 with open("input.txt", "r") as f:
     input = f.read()
-    
+
 test_input = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -13,30 +16,52 @@ def solution_1(input):
     res = 0
     input.strip()
     for game in input.splitlines():
-        solution, trial = game.split(" | ")[0].split(": ")[-1].split(" "), game.split("|")[1].strip().split(" "), 
-        
+        solution, trial = (
+            game.split(" | ")[0].split(": ")[-1].split(" "),
+            game.split("|")[1].strip().split(" "),
+        )
+
         solution = [int(x) for x in solution if x != ""]
         trial = [int(x) for x in trial if x != ""]
-        print(solution, trial)
+
         curr_res = 0
-        for t in trial :
-            if t in solution :
-                print(t)
-                if curr_res == 0 :
+        for t in trial:
+            if t in solution:
+                if curr_res == 0:
                     curr_res = 1
                 else:
                     curr_res *= 2
-                
+
         res += curr_res
-        
+
     print("Solution 1: ")
     print(res)
 
 
-def solution_2():
+def solution_2(input):
+    input.strip()
+    card_count = defaultdict(int)
+    for idx, game in enumerate(input.splitlines()):
+        solution, trial = (
+            game.split(" | ")[0].split(": ")[-1].split(" "),
+            game.split("|")[1].strip().split(" "),
+        )
+
+        solution = [int(x) for x in solution if x != ""]
+        trial = [int(x) for x in trial if x != ""]
+
+        x = 0
+        for t in trial:
+            if t in solution:
+                x += 1
+        card_count[idx] += 1
+        for i in range(x):
+            card_count[idx + i + 1] += card_count[idx]
+
     print("Solution 2: ")
+    print(sum(card_count.values()))
 
 
 if __name__ == "__main__":
     solution_1(input)
-    solution_2()
+    solution_2(input)
