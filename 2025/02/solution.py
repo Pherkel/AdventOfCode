09bd021512ad75347bb11ef6ed10a1811ea7b38f
Pherkel <p.merkel@optocycle.com>
2025-12-02 10:10:18 +0100

    2025/02/02

--- 2025/02/solution.py
+++ 2025/02/solution.py
@@ -13,3 +13,20 @@ for id_range in id_ranges:
             invalid_ids.append(id)
 
 print(sum(invalid_ids))
+
+invalid_ids = []
+for id_range in id_ranges:
+    for id in range(id_range[0], id_range[1] + 1):
+        id_str = str(id)
+        length = len(id_str)
+        is_invalid = False
+        for pattern_len in range(1, length // 2 + 1):
+            if length % pattern_len == 0:
+                pattern = id_str[:pattern_len]
+                if pattern * (length // pattern_len) == id_str:
+                    is_invalid = True
+                    break
+        if is_invalid:
+            invalid_ids.append(id)
+
+print(sum(invalid_ids))
